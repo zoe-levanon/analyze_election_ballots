ccefc5e66abdd10b7026dd45e0858ac2ce182ebb
ozlev <oz.levanon@gmail.com>
2021-03-28 21:55:17 +0300

    Added ESLint (and fixes resulting from it)

--- src/analyzeBallots.ts
+++ src/analyzeBallots.ts
@@ -1,61 +1,60 @@
-import { accumulateBy, ElectionsData, readElections, SingleBallotData } from "./utils";
+import { ElectionsData, readElections, SingleBallotData } from './utils'
 
-const prevResults = readElections("./resources/2020_03_02/expb.csv");
-const results = readElections("./resources/2021_03_23/expb.csv");
-const report = detectSuspiciousBallotsByTotalResults(results);
-// console.log(report);
+// const prevResults = readElections('./resources/2020_03_02/expb.csv')
+const results = readElections('./resources/2021_03_23/expb.csv')
+const report = detectSuspiciousBallotsByTotalResults(results)
+console.log(report)
 
-function detectSuspiciousBallotsByTotalResults(results: ElectionsData): string {
-    let loss: Record<string, number> = {};
-    let gain: Record<string, number> = {};
-    let report = "Location,Location ID,Ballot ID,Number of Votes,Counted As,Should Be\n";
-    results.ballots.forEach(b => {
-        for (let i = 0; i < 3; i++) {
-            let ballotNthParty = b.ordered.byPlace[i];
-            let nationalPositionOfParty = results.totals.ordered.byParty[ballotNthParty];
-            let voteCount = b.votes[ballotNthParty];
-            if (nationalPositionOfParty > 12 + i && voteCount > 2) {
-                // See if there's a nationally high-ranked party that didn't receive any votes here...
-                let partiesWithZeroVotes: string[] = [];
-                for (let i = 0; i < 4; i++) {
-                    let nationalPartyAtN = results.totals.ordered.byPlace[i];
-                    if (b.votes[nationalPartyAtN] === 0) {
-                        partiesWithZeroVotes.push(nationalPartyAtN);
-                    }
-                }
-
-                if (partiesWithZeroVotes.length === 0) {
-                    console.log(`Ballot ${id(b)} (${b.municipalityName}): ${party(ballotNthParty)} is in position ${i + 1} (${voteCount} votes) but nationally at position ${nationalPositionOfParty}`);
-                } else {
-                    console.log(`Ballot ${id(b)} (${b.municipalityName}): votes potentially recorded to ${party(ballotNthParty)} (${voteCount} votes) instead of ${partiesWithZeroVotes} (with 0 votes)`);
-                    if (partiesWithZeroVotes.length === 1 && voteCount > 10) {
-                        report += `${b.municipalityName},${b.municipalityId},${b.ballotId},${voteCount},${ballotNthParty},${partiesWithZeroVotes[0]}\n`;
-                    }
-                }
-
-                loss[ballotNthParty] = (loss[ballotNthParty] || 0) + voteCount;
-                partiesWithZeroVotes.forEach(p => {
-                    gain[p] = (gain[p] || 0) + voteCount;
-                });
-            }
+function detectSuspiciousBallotsByTotalResults (results: ElectionsData): string {
+  const loss: Record<string, number> = {}
+  const gain: Record<string, number> = {}
+  let report = 'Location,Location ID,Ballot ID,Number of Votes,Counted As,Should Be\n'
+  results.ballots.forEach(b => {
+    for (let i = 0; i < 3; i++) {
+      const ballotNthParty = b.ordered.byPlace[i]
+      const nationalPositionOfParty = results.totals.ordered.byParty[ballotNthParty]
+      const voteCount = b.votes[ballotNthParty]
+      if (nationalPositionOfParty > 12 + i && voteCount > 2) {
+        // See if there's a nationally high-ranked party that didn't receive any votes here...
+        const partiesWithZeroVotes: string[] = []
+        for (let i = 0; i < 4; i++) {
+          const nationalPartyAtN = results.totals.ordered.byPlace[i]
+          if (b.votes[nationalPartyAtN] === 0) {
+            partiesWithZeroVotes.push(nationalPartyAtN)
+          }
         }
-    });
 
+        if (partiesWithZeroVotes.length === 0) {
+          console.log(`Ballot ${id(b)} (${b.municipalityName}): ${party(ballotNthParty)} is in position ${i + 1} (${voteCount} votes) but nationally at position ${nationalPositionOfParty}`)
+        } else {
+          console.log(`Ballot ${id(b)} (${b.municipalityName}): votes potentially recorded to ${party(ballotNthParty)} (${voteCount} votes) instead of ${partiesWithZeroVotes} (with 0 votes)`)
+          if (partiesWithZeroVotes.length === 1 && voteCount > 10) {
+            report += `${b.municipalityName},${b.municipalityId},${b.ballotId},${voteCount},${ballotNthParty},${partiesWithZeroVotes[0]}\n`
+          }
+        }
 
-    /*Object.keys(loss).forEach(k => {
+        loss[ballotNthParty] = (loss[ballotNthParty] || 0) + voteCount
+        partiesWithZeroVotes.forEach(p => {
+          gain[p] = (gain[p] || 0) + voteCount
+        })
+      }
+    }
+  })
+
+  /* Object.keys(loss).forEach(k => {
         console.log(`${party(k)} loses: ${loss[k]}`);
     });
     Object.keys(gain).forEach(k => {
         console.log(`${party(k)} gains: ${gain[k]}`);
-    });*/
+    }); */
 
-    return report;
+  return report
 }
 
-function id(b: SingleBallotData) {
-    return `${b.municipalityId}/${b.ballotId}`;
+function id (b: SingleBallotData) {
+  return `${b.municipalityId}/${b.ballotId}`
 }
 
-function party(id: string): string {
-    return id;
+function party (id: string): string {
+  return id
 }
--- src/utils.ts
+++ src/utils.ts
@@ -1,17 +1,9 @@
-import * as fs from "fs";
+import * as fs from 'fs'
 
-const parse = require("csv-parse/lib/sync");
-const iconv = require('iconv-lite');
+const parse = require('csv-parse/lib/sync')
+const iconv = require('iconv-lite')
 
-const COUNTRY_ID = "Country";
-
-export type ElectionsData = {
-    ballots: Array<SingleBallotData>;
-    byBallot: Record<string, CumulativeBallotData>;
-    byBallotGroup: Record<string, CumulativeBallotData>;
-    byMunicipality: Record<string, CumulativeBallotData>;
-    totals: CumulativeBallotData;
-}
+const COUNTRY_ID = 'Country'
 
 export type OrderedData = {
     byParty: Record<string, number>;
@@ -37,26 +29,34 @@ export type CumulativeBallotData = VotingData & {
     id: string;
 }
 
-export function readElections(file: string): ElectionsData {
-    const ballots = readBallots(file);
-    const byBallot = accumulateBy(ballots, b => `${b.municipalityId}/${b.ballotId}`);
-    const byBallotGroup = accumulateBy(ballots, b => {
-        let ballotId = b.ballotId;
-        let firstPeriod = ballotId.indexOf('.');
-        if (firstPeriod > 0) {
-            ballotId = ballotId.substring(0, firstPeriod);
-        }
-        return `${b.municipalityId}/${ballotId}`;
-    });
-    const byMunicipality = accumulateBy(ballots, b => `${b.municipalityId}`);
-    const totals = accumulateBy(ballots, () => COUNTRY_ID)[COUNTRY_ID];
-    return {
-        ballots,
-        byBallot,
-        byBallotGroup,
-        byMunicipality,
-        totals
+export type ElectionsData = {
+    ballots: Array<SingleBallotData>;
+    byBallot: Record<string, CumulativeBallotData>;
+    byBallotGroup: Record<string, CumulativeBallotData>;
+    byMunicipality: Record<string, CumulativeBallotData>;
+    totals: CumulativeBallotData;
+}
+
+export function readElections (file: string): ElectionsData {
+  const ballots = readBallots(file)
+  const byBallot = accumulateBy(ballots, b => `${b.municipalityId}/${b.ballotId}`)
+  const byBallotGroup = accumulateBy(ballots, b => {
+    let ballotId = b.ballotId
+    const firstPeriod = ballotId.indexOf('.')
+    if (firstPeriod > 0) {
+      ballotId = ballotId.substring(0, firstPeriod)
     }
+    return `${b.municipalityId}/${ballotId}`
+  })
+  const byMunicipality = accumulateBy(ballots, b => `${b.municipalityId}`)
+  const totals = accumulateBy(ballots, () => COUNTRY_ID)[COUNTRY_ID]
+  return {
+    ballots,
+    byBallot,
+    byBallotGroup,
+    byMunicipality,
+    totals
+  }
 }
 
 /**
@@ -64,96 +64,96 @@ export function readElections(file: string): ElectionsData {
  * @param file file to read from (either local of s3)
  * @returns array of rows (key=value map) in CSV file
  */
-export function readBallots(file: string): Array<SingleBallotData> {
-    // Read CSV file as binary
-    let buffer = fs.readFileSync(file, { encoding: "binary" });
+export function readBallots (file: string): Array<SingleBallotData> {
+  // Read CSV file as binary
+  const buffer = fs.readFileSync(file, { encoding: 'binary' })
 
-    // Convert line 0 from ISO-8859-8 to standard UTF
-    let contents = buffer.toString();
-    let lines = contents.split("\n").map(l => iconv.decode(l, "ISO-8859-8"));
-    const sBuffer = Buffer.from(lines.join("\n"), "utf8");
+  // Convert line 0 from ISO-8859-8 to standard UTF
+  const contents = buffer.toString()
+  const lines = contents.split('\n').map(l => iconv.decode(l, 'ISO-8859-8'))
+  const sBuffer = Buffer.from(lines.join('\n'), 'utf8')
 
-    // Parse the CSV
-    let result: Array<string[]> = parse(sBuffer, { trim: true });
-    let columnNames: string[] = result.shift()!;
-    // Remove empty header columns
-    while (columnNames[columnNames.length - 1] === "") {
-        columnNames.pop();
-    }
-    return result.map((r: any) => processLine(r, columnNames));
+  // Parse the CSV
+  const result: Array<string[]> = parse(sBuffer, { trim: true })
+  const columnNames: string[] = result.shift()!
+  // Remove empty header columns
+  while (columnNames[columnNames.length - 1] === '') {
+    columnNames.pop()
+  }
+  return result.map((r: any) => processLine(r, columnNames))
 }
 
-function sortVotes(votes: Record<string, number>): OrderedData {
-    const byParty: Record<string, number> = {};
-    const byPlace: string[] = [];
-    Object.keys(votes).sort((v1, v2) => votes[v2] - votes[v1]).forEach((v, i) => {
-        byParty[v] = i;
-        byPlace.push(v);
-    })
-    return { byParty, byPlace };
+function sortVotes (votes: Record<string, number>): OrderedData {
+  const byParty: Record<string, number> = {}
+  const byPlace: string[] = []
+  Object.keys(votes).sort((v1, v2) => votes[v2] - votes[v1]).forEach((v, i) => {
+    byParty[v] = i
+    byPlace.push(v)
+  })
+  return { byParty, byPlace }
 }
 
-function processLine(line: string[], columnNames: string[]): SingleBallotData {
-    let ballotVotes: Record<string, number> = {};
-    for (let i = 11; i < columnNames.length; i++) {
-        ballotVotes[columnNames[i]] = parseInt(line[i]);
-    }
-    return {
-        municipalityName: line[2],
-        municipalityId: line[3],
-        ballotId: line[4],
-        voters: parseInt(line[7]),
-        totalVotes: parseInt(line[8]),
-        disqualified: parseInt(line[9]),
-        validVotes: parseInt(line[10]),
-        votes: ballotVotes,
-        ordered: sortVotes(ballotVotes)
-    }
+function processLine (line: string[], columnNames: string[]): SingleBallotData {
+  const ballotVotes: Record<string, number> = {}
+  for (let i = 11; i < columnNames.length; i++) {
+    ballotVotes[columnNames[i]] = parseInt(line[i])
+  }
+  return {
+    municipalityName: line[2],
+    municipalityId: line[3],
+    ballotId: line[4],
+    voters: parseInt(line[7]),
+    totalVotes: parseInt(line[8]),
+    disqualified: parseInt(line[9]),
+    validVotes: parseInt(line[10]),
+    votes: ballotVotes,
+    ordered: sortVotes(ballotVotes)
+  }
 }
 
-export function accumulateBy(ballots: Array<SingleBallotData>, idFunc: (b: SingleBallotData) => string): Record<string, CumulativeBallotData> {
-    // Group by ID
-    const byId = ballots.reduce((prev: any, cur: SingleBallotData) => {
-        let id = idFunc(cur);
-        let ballots = prev[id] || [];
-        ballots.push(cur);
-        prev[id] = ballots;
-        return prev;
-    }, {});
+export function accumulateBy (ballots: Array<SingleBallotData>, idFunc: (b: SingleBallotData) => string): Record<string, CumulativeBallotData> {
+  // Group by ID
+  const byId = ballots.reduce((prev: any, cur: SingleBallotData) => {
+    const id = idFunc(cur)
+    const ballots = prev[id] || []
+    ballots.push(cur)
+    prev[id] = ballots
+    return prev
+  }, {})
 
-    // Accumulate votes by ID
-    let accumulations: Record<string, CumulativeBallotData> = {};
-    Object.keys(byId).forEach(k => {
-        const ballots: Array<SingleBallotData> = byId[k];
-        const cumulate: CumulativeBallotData = {
-            id: k,
-            voters: 0,
-            validVotes: 0,
-            disqualified: 0,
-            totalVotes: 0,
-            votes: {},
-            ordered: { byPlace: [], byParty: {} }
-        };
-        ballots.forEach(b => addVotingData(b, cumulate));
-        accumulations[k] = cumulate;
-    })
-
-    for (let k in accumulations) {
-        if (accumulations.hasOwnProperty(k)) {
-            accumulations[k].ordered = sortVotes(accumulations[k].votes);
-        }
+  // Accumulate votes by ID
+  const accumulations: Record<string, CumulativeBallotData> = {}
+  Object.keys(byId).forEach(k => {
+    const ballots: Array<SingleBallotData> = byId[k]
+    const cumulate: CumulativeBallotData = {
+      id: k,
+      voters: 0,
+      validVotes: 0,
+      disqualified: 0,
+      totalVotes: 0,
+      votes: {},
+      ordered: { byPlace: [], byParty: {} }
     }
+    ballots.forEach(b => addVotingData(b, cumulate))
+    accumulations[k] = cumulate
+  })
 
-    return accumulations;
+  for (const k in accumulations) {
+    if (Object.prototype.hasOwnProperty.call(accumulations, k)) {
+      accumulations[k].ordered = sortVotes(accumulations[k].votes)
+    }
+  }
+
+  return accumulations
 }
 
-export function addVotingData(cur: SingleBallotData, cumulate: CumulativeBallotData) {
-    cumulate.disqualified += cur.disqualified;
-    cumulate.totalVotes += cur.totalVotes;
-    cumulate.validVotes += cur.validVotes;
-    cumulate.voters += cur.voters;
-    const parties = Object.keys(cur.votes);
-    parties.forEach(p => {
-        cumulate.votes[p] = (cumulate.votes[p] || 0) + (cur.votes[p] || 0);
-    })
+export function addVotingData (cur: SingleBallotData, cumulate: CumulativeBallotData) {
+  cumulate.disqualified += cur.disqualified
+  cumulate.totalVotes += cur.totalVotes
+  cumulate.validVotes += cur.validVotes
+  cumulate.voters += cur.voters
+  const parties = Object.keys(cur.votes)
+  parties.forEach(p => {
+    cumulate.votes[p] = (cumulate.votes[p] || 0) + (cur.votes[p] || 0)
+  })
 }
